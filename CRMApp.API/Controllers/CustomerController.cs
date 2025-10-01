99fa053f3ac25687666826249311dc61a5119ea1
Dinushantha <58838308+Dinushantha@users.noreply.github.com>
2025-10-01 21:13:50 +0530

    Add files via upload

---
+++ CRMApp.API/Controllers/CustomerController.cs
@@ -0,0 +1,57 @@
+﻿using CRMApp.Application.DTOs;
+using CRMApp.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CRMApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly CustomerService _service;
+
+        public CustomerController(CustomerService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<CustomerDto>>> GetAll()
+        {
+            var customers = await _service.GetAllAsync();
+            return Ok(customers);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CustomerDto>> GetById(int id)
+        {
+            var customer = await _service.GetByIdAsync(id);
+            if (customer == null) return NotFound();
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Add(CustomerDto dto)
+        {
+            await _service.AddAsync(dto);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, CustomerDto dto)
+        {
+            dto.Id = id;
+            await _service.UpdateAsync(dto);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _service.DeleteAsync(id);
+            return Ok();
+        }
+    }
+}
